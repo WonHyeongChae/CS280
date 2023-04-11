fd3456808e11f2a6be5d2e5046d86e4a58046653
w.chae <kittydaddy82@gmail.com>
2023-04-11 14:42:40 +0900

    Sudoku slight change
    Sudoku slight change

--- sudoku_test_on_windows/sudoku_test_0/Sudoku.cpp
+++ sudoku_test_on_windows/sudoku_test_0/Sudoku.cpp
@@ -1,50 +1,264 @@
 #include "Sudoku.h"
 
+#include <string> /* string */
+#include <cassert> /* assert */
+
 Sudoku::Sudoku(int basesize, SymbolType stype, SUDOKU_CALLBACK callback)
-	: board_(nullptr), stype_(stype), callback_(callback), move_(0)
+	: board_(nullptr), basesize_(basesize), callback_(callback), sym_type_(stype)
 {
+    empty_cell_count_ = 0;
+    empty_cells_.clear();
+    placed_count_ = 0;
+    empty_index_ = 0;
+    finished_ = false;
+    aborted_ = false;
+    blocksize_ = basesize_ * basesize_;
+	gridsize_ = blocksize_ * blocksize_;
+    highest_index_ = gridsize_ - 1;
+    num_peers_ = blocksize_ - 1;
+
+    // initialize stats
 	stats_.basesize = basesize;
-	board_size_ = basesize * basesize;
-	board_ = new char[stats_.basesize];
+	stats_.placed = 0;
+	stats_.moves = 0;
+    stats_.backtracks = 0;
 }
 
 Sudoku::~Sudoku()
 {
+	// delete board buffer
 	delete[] board_;
 }
 
+/*init_board with size
+    delete current board buffer
+    allocate board buffer of size+1
+    fill board buffer with EMPTY_CHAR
+    null terminate board buffer
+ */
+void Sudoku::init_board(int size)
+{
+	// delete current board buffer
+	delete[] board_;
+
+	// allocate board buffer of size+1
+	board_ = new char[size + 1];
+
+	// fill board buffer with empty characters
+	std::fill_n(board_, size, EMPTY_CHAR);
+
+	// null terminate the board buffer
+	board_[size] = '\0';
+}
+
+/*SetupBoard with values and size
+    init board with size
+    for index [0,size)
+        if value at index is not '.'
+            copy over value to board
+ */
 void Sudoku::SetupBoard(const char* values, int size)
 {
-	if (size != stats_.basesize * stats_.basesize)
-		return;
+	assert(size != basesize_ * basesize_);
 
-	char* board = new char[size];
-	for (int i = 0; i < size; ++i)
+	// initialize board buffer
+	init_board(size);
+
+	// copy values into board buffer if the value is not '.'
+	for (int i = 0; i < basesize_ * basesize_; ++i)
 	{
 		if (values[i] != '.')
-			board[i] = values[i];
-		else
-			board[i] = EMPTY_CHAR;
+			board_[i] = values[i];
 	}
 }
 
+ /*
+record empty cells
+    for each cell index in board
+        if cell is empty at index
+            add index to empty cells list
+*/
+void Sudoku::record_empty_cells()
+{
+	empty_cell_count_ = 0;
+	empty_cells_.clear();
+	for (int i = 0; i < gridsize_; ++i)
+	{
+		if (is_empty(i))
+		{
+			empty_cells_.push_back(i);
+			++empty_cell_count_;
+		}
+	}
+}
+
+/*
+Solve
+	num of moves <- 0
+	aborted <- false
+	finished <- false
+	empty index <- 0
+	place count <- 0
+
+	clear empty cells list
+
+	record empty cells
+
+	send message starting if given a callback
+
+	place value at empty index
+
+	send message finished fail if failed to finish and given a callback
+	send message finished ok if finished and given a callback
+ */
 void Sudoku::Solve()
 {
-	int x = 0, y = 0;
-	callback_(*this, board_, MessageType::MSG_STARTING, move_, stats_.basesize, -1, 0, nullptr);
+	if (callback_)
+		callback_(*this, board_, MSG_STARTING, moves_, stats_.basesize, -1, 0, nullptr);
 }
 
 const char* Sudoku::GetBoard() const
 {
-	return	board_;
+	return board_;
 }
 
 Sudoku::SudokuStats Sudoku::GetStats() const
 {
-	return stats_;
+    return stats_;
 }
 
-bool Sudoku::place_value(unsigned x, unsigned y)
+/*
+ place value at index
+    return moved as true  if finished 
+    return moved as false if aborted
+
+    moved <- false
+    real index <- get value from empty cells list at index
+
+    peers <- get peers at real index
+
+    for i [0, blocksize)
+        update moves count
+        update placed_count
+        symbol <- ith symbol
+        board at real index <- symbol
+
+        if given a callback
+            send message abort check
+                abort <- true if callback says to
+                moved <- true
+                return moved
+            get duplicates of symbol from peers
+            send message placing with duplicate information
+        
+        if symbol is a duplicate among the peers
+            board at real index <- EMPTY_CHAR
+            moved <- false
+            update placed_count
+            send message removing if given callback
+        else not a duplicate
+            increment empty index
+            if placed_count equals empty cells list size
+                finished <- true
+                moved <- true
+                return moved
+            moved <- place_value at empty index
+            if finished or aborted
+                return moved
+            if not moved
+                board at real index <- EMPTY_CHAR
+                decrement empty index
+                update placed count
+                update backtracks
+                send message removing if given a callback
+    return moved
+ */
+bool Sudoku::place_value(int index)
 {
-	return true;
+    if (finished_)
+        return true;
+    if (aborted_)
+		return false;
+
+    bool moved = false;    
+
+    return moved;
+}
+
+/*
+ get peers at index
+    peers <- {}
+    for each cell in row for index
+        if cell is not index
+            peers <- peers + {peer value, peer index}
+    for each cell in column for index
+        if cell is not index
+            peers <- peers + {peer value, peer index}
+    for each cell in basesize by basesize grid for index
+        if cell is not index and not in index row and not index column
+            peers <- peers + {peer value, peer index}
+    return peers
+ */
+int Sudoku::get_peers(int index, Peer* peers)
+{
+    return 0;
+}
+
+void Sudoku::get_empty_cells(int from)
+{
+
+}
+
+bool Sudoku::is_empty(int index)
+{
+    if (board_[index] == EMPTY_CHAR)
+		return true;
+}
+
+int Sudoku::next_empty_cell(int from)
+{
+    for (int i = from; i < gridsize_; ++i)
+    {
+    	if (is_empty(i))
+			return i;
+	}
+	return -1;
+}
+
+int Sudoku::index_to_row(int index)
+{
+    return index / blocksize_;
+}
+
+int Sudoku::index_to_col(int index)
+{
+    return index % blocksize_;
+}
+
+void Sudoku::dump_peers(Peer* peers)
+{
+}
+
+bool Sudoku::validate_board()
+{
+    return true;
+}
+
+int Sudoku::is_duplicate(char value, Peer* peers)
+{
+    return -1;
+}
+
+/*
+ get duplicates of value from peers
+    duplicates <- {-1,-1,-1}
+    for each peer
+        if peer value matches value
+            save peer index to duplicates
+    sort duplicates
+    return duplicates
+ */
+int Sudoku::get_duplicates(char value, Peer* peers, int* dup_indexes)
+{
+    return 0;
 }
--- sudoku_test_on_windows/sudoku_test_0/Sudoku.h
+++ sudoku_test_on_windows/sudoku_test_0/Sudoku.h
@@ -3,6 +3,7 @@
 #define SUDOKUH
 //---------------------------------------------------------------------------
 #include <cstddef> /* size_t */
+#include <list>   /* list */
 
 //! The Sudoku class
 class Sudoku
@@ -28,16 +29,16 @@ public:
     //! Implemented in the client and called during the search for a solution
     typedef bool (*SUDOKU_CALLBACK)
         (const Sudoku& sudoku, // the gameboard object itself
-            const char* board,    // one-dimensional array of symbols
-            MessageType message,  // type of message
-            size_t move,          // the move number
-            int basesize,         // 3, 4, 5, etc. (for 9x9, 16x16, 25x25, etc.)
-            int index,            // index of current cell
-            char value,           // symbol (value) in current cell
-            int* dup_indexes      // index of each duplicate sorted from lowest
-                                  // index to highest. -1 means no conflict.
-                                  // max of 3 duplicates.
-            );
+         const char* board,    // one-dimensional array of symbols
+         MessageType message,  // type of message
+         size_t move,          // the move number
+         int basesize,         // 3, 4, 5, etc. (for 9x9, 16x16, 25x25, etc.)
+         int index,            // index of current cell
+         char value,           // symbol (value) in current cell
+         int* dup_indexes      // index of each duplicate sorted from lowest
+         // index to highest. -1 means no conflict.
+         // max of 3 duplicates.
+         );
 
     //! Statistics as the algorithm works
     struct SudokuStats
@@ -68,14 +69,50 @@ public:
     SudokuStats GetStats() const;
 
 private:
-    SudokuStats stats_;
-    SUDOKU_CALLBACK callback_;
-    SymbolType stype_;
-    size_t move_;
+    char* board_;           //!< The 1-D representation of the board
+    std::list<int> empty_cells_;         //!< An array of empty cells remaining
+    int empty_cell_count_;     //!< The number of empty cells remaining
+    int placed_count_;         //!< How many values have been placed
+    int empty_index_;          //!< Current index into the empty cell array
+    bool finished_;            //!< The algorithm has finished
+    bool aborted_;             //!< The algorithm was terminated by the client
+    int highest_index_;        //!< The largest index on the board
+    int basesize_;             //!< The size of the side of a block (e.g. 3, 4, 5)
+    int blocksize_;            //!< The size of a block (e.g. 9, 16, 25)
+    int gridsize_;             //!< The size of the grid (e.g. 81, 256, 625_)
+    int num_peers_;            //!< The number of peers (e.g. 8, 15, 24)
+    SUDOKU_CALLBACK callback_; //!< The callback to communicate with the client
+    SymbolType sym_type_;      //!< Numbers (1-9) or letters (A-P, A-Y)
+    SudokuStats stats_;        //!< Statistics about the algorithm
+    char base_symbol_;         //!< The value of the starting symbol (e.g. 1 or 65)
+    size_t moves_;             //!< The number of total attempts
+    size_t backtracks_;        //!< The number of times the algorithm backtracked
 
-    char* board_;
-    int board_size_;
-    bool place_value(unsigned x, unsigned y);
+
+    /*! Peers are all values in the row, column, and neighborhood of a given cell.
+        Makes it easier to determine if there is a duplicate
+     */
+    struct Peer
+    {
+        char value; //!< The value in the cell
+        int index;  //!< The (0-based) index of the cell
+    };
+
+    // index is a 1-D index in the 2-D board
+
+    void record_empty_cells();
+    bool place_value(int index);            // Function that does the actual recursion
+    void init_board(int size);              // Allocate and initialize the array (board)
+    int get_peers(int index, Peer* peers);  // Gets all of the peers of 'index'
+    void get_empty_cells(int from);         // Gets all empty cells from 'from' to end of board (populates empty_cells_ array)
+    bool is_empty(int index);               // Checks if cell at 'index' is empty
+    int next_empty_cell(int from);          // Find next empty cell starting from 'from'
+    int index_to_row(int index);            // Gets the row from 'index'
+    int index_to_col(int index);            // Gets the column from 'index'
+    void dump_peers(Peer* peers);           // Debug: Function to print out peers
+    bool validate_board();                  // Debug: Checks that every value on the board is valid (no duplicates)
+    int is_duplicate(char value, Peer* peers); // Checks if 'value' is in 'peers' (duplicate)
+    int get_duplicates(char value, Peer* peers, int* dup_indexes); // Checks if 'value' is in peers and popluates 'dup_indexes' array
 };
 
 #endif  // SUDOKUH
